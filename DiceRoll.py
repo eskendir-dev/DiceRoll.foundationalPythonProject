17190ee145ea4e2a0b961c585bcce3e018ef7deb
eskendir <eskendir2025@gmail.com>
2025-12-07 12:24:32 +0500

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -4,7 +4,6 @@ while True:
     try:
         number_of_dices = int(input('How many dices? '))
         break
-        
     except ValueError:
         print("Error: Please enter a valid whole number. Try again.")
 
@@ -12,5 +11,6 @@ result = 0
 
 for i in range(number_of_dices): 
     result += random.randint(1, 6)
+
 
 print('Total Result: ', result)
